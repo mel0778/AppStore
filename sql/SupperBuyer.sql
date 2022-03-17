b45da8f4bce5e0e17cb939d4a5fd2ae4e8739de1
micolec <86700792+micolec@users.noreply.github.com>
2022-03-17 23:05:46 +0800

    Update SupperBuyer.sql

--- sql/SupperBuyer.sql
+++ sql/SupperBuyer.sql
@@ -1,4 +1,4 @@
 -- populate buyers table
 
-INSERT INTO buyer VALUES('micole88', '123', 'micole', 87653454, 'RH', 5);
-INSERT INTO buyer VALUES('mel123', '@Lk', 'melody', 96853228, 'RH', 5);
+INSERT INTO buyer VALUES('micole88', '123', 'Micole', 'Chan', 87653454, 'RH', 5);
+INSERT INTO buyer VALUES('mel123', '@Lk', 'Melody', 'Tan' 96853228, 'RH', 5);
